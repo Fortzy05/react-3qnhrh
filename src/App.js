c175fd9531f20eb7628ccacbb09472e3064bc23f
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-09-13 12:11:11 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
-import React, { useState } from 'react';
-import './style.css';
-import ContactForm from './components/ContactForm.jsx';
+// import React, { useState } from 'react';
+// import './style.css';
+// import ContactForm from './components/ContactForm.jsx';
 
-export default function App() {
-  const [contacts, updateContacts] = useState([]);
+// export default function App() {
+//   const [contacts, updateContacts] = useState([]);
 
-  const addContact = (contactInfo) => {
-    updateContacts([...contacts, contactInfo]);
-  };
-  console.log(contacts);
-  return <ContactForm addContact={addContact} />;
+//   const addContact = (contactInfo) => {
+//     updateContacts([...contacts, contactInfo]);
+//   };
+//   console.log(contacts);
+//   return <ContactForm addContact={addContact} />;
+// }
+
+import React from "react";
+
+const products = [{
+  title: 'cabbage', id: 1
+},
+{
+
+    title: 'Garlic', id: 2
+  },
+  {
+    title: 'Apple', id: 3
+  } ];
+
+
+  const listItem = products.map(product => <li key={product.id}>{product.title}</li>)
+
+
+
+const App = () =>{
+  return <ol>{listItem}</ol>
 }
+
+
+
+export default App;
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,53 +1,53 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
- const ContactForm = ({ addContact }) => {
-  const [contactInfo, setContactInfo] = useState({
-    username: '',
-    email: '',
-    phoneNumber: '',
-  });
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    addContact(contactInfo);
-    setContactInfo({ username: '', email: '', phoneNumber: '' });
-  };
+//  const ContactForm = ({ addContact }) => {
+//   const [contactInfo, setContactInfo] = useState({
+//     username: '',
+//     email: '',
+//     phoneNumber: '',
+//   });
+//   const handleFormSubmit = (event) => {
+//     event.preventDefault();
+//     addContact(contactInfo);
+//     setContactInfo({ username: '', email: '', phoneNumber: '' });
+//   };
 
-  const handleInputChange = (event) => {
-    setContactInfo({...contactInfo, [event.target.name]: event.target.value });
-  };
-  return (
-    <section>
-    <form onSubmit={handleFormSubmit} className="form-style">
-      <label>
-        Username:
-        <input
-          value={contactInfo.username}
-          onChange={handleInputChange}
-          type="text"
-          name="username"
-        />
-      </label>
-      <label>
-        Email:
-        <input
-          value={contactInfo.email}
-          onChange={handleInputChange}
-          type="email"
-          name="email"
-        />
-      </label>
-      <label>
-        Phone Number:
-        <input
-          value={contactInfo.phoneNumber}
-          onChange={handleInputChange}
-          type="number"
-          name="phoneNumber"
-        />
-      </label>
-      <button className="btn">Submit</button>
-    </form>
-    </section>
-  );
-};
-export default ContactForm;
+//   const handleInputChange = (event) => {
+//     setContactInfo({...contactInfo, [event.target.name]: event.target.value });
+//   };
+//   return (
+//     <section>
+//     <form onSubmit={handleFormSubmit} className="form-style">
+//       <label>
+//         Username:
+//         <input
+//           value={contactInfo.username}
+//           onChange={handleInputChange}
+//           type="text"
+//           name="username"
+//         />
+//       </label>
+//       <label>
+//         Email:
+//         <input
+//           value={contactInfo.email}
+//           onChange={handleInputChange}
+//           type="email"
+//           name="email"
+//         />
+//       </label>
+//       <label>
+//         Phone Number:
+//         <input
+//           value={contactInfo.phoneNumber}
+//           onChange={handleInputChange}
+//           type="number"
+//           name="phoneNumber"
+//         />
+//       </label>
+//       <button className="btn">Submit</button>
+//     </form>
+//     </section>
+//   );
+// };
+// export default ContactForm;
