fab59a837ab7c9a0ce9b32389e680db27f2cb900
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-09-13 12:37:24 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@
 import React from "react";
 
 const products = [{
-  title: 'cabbage', isFruit: false, id: 1
+  title: 'Cabbage', isFruit: false, id: 1
 },
 {
 
-    title: 'Garlic',isFruit: false, id: 2
+    title: 'Garlic', isFruit: false, id: 2
   },
   {
-    title: 'Apple',isFruit: true, id: 3
+    title: 'Apple', isFruit: true, id: 3
   } ];
 
 
