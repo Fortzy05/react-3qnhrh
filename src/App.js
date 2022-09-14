bbd98689c56286d6f9561d7a5e95351320cd7402
Fortzy05 <39365492+Fortzy05@users.noreply.github.com>
2022-09-14 17:46:01 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -18,8 +18,8 @@ import FilterableProductTable from "./components/FilterableProductTable"
 
 const PRODUCTS = [
   { category: "Fruits", price: "$1", stocked: true, name: "Apple" },
-  { category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit" },
-  { category: "Fruits", price: "$2", stocked: false, name: "Passionfruit" },
+  { category: "Fruits", price: "$1", stocked: true, name: "Dragon Fruit" },
+  { category: "Fruits", price: "$2", stocked: false, name: "Passion Fruit" },
   { category: "Vegetables", price: "$2", stocked: true, name: "Spinach" },
   { category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin" },
   { category: "Vegetables", price: "$1", stocked: true, name: "Peas" }
